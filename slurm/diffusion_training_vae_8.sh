0ba482954524545d8cbf0505cd4bac4bd1691edc
Erik Iamlis <erik.schuetz@hm.edu>
2026-03-24 16:57:46 +0100

    config and job description changes.

---
+++ slurm/diffusion_training_vae_8.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH --job-name=new_vae_stoch
+#SBATCH --output=/mnt/md0/erik/outputs/diffusion_train_%j.txt
+#SBATCH --time=3-00:00:00
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:a6000:1
+#SBATCH --export=NONE
+
+unset SLURM_EXPORT_ENV
+
+source ~/anaconda3/etc/profile.d/conda.sh
+
+conda activate mld
+
+cd /mnt/md0/erik/motion-latent-diffusion-1
+
+export OMP_NUM_THREADS=12
+export WANDB_API_KEY="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"
+
+python -m train \
+--cfg configs/config_ego_motion_vae.yaml \
+--nodebug
